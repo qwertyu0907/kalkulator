140b38e3d10e2c336697d605378f2b851ff8e750
qwertytrewq123 <david0072004@gmail.com>
2018-12-01 17:52:50 +0300

    lol

---
+++ Разделить.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string A2, B2;
+                int A, B, C;
+                Console.WriteLine(
+                    "Введите Первое Число");
+                A2 = Console.ReadLine();
+                Console.WriteLine("Введите Второе Число");
+                B2 = Console.ReadLine();
+                A = Int32.Parse(A2);
+                B = Int32.Parse(B2);
+                C = A / B;
+                Console.WriteLine("Получилось: " + C + "!");
+            }
+        }
+    }
+}
